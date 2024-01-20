4e9f652241e02977504a5fcf106835d5e2afd84a
Mrkiller2001 <57162352+Mrkiller2001@users.noreply.github.com>
2024-01-20 16:38:05 +1000

    Now displays meta tags on site.

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -4,47 +4,48 @@ import { MetaTags } from "@/types";
 const { JSDOM } = require("jsdom");
 
 async function getHtml(url: string): Promise<string | null> {
-  try {
-    const response = await fetch(url);
+    try {
+        const response = await fetch(url);
 
-    if (!response.ok) {
-      throw new Error(`Failed to fetch: ${response.statusText}`);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch: ${response.statusText}`);
+        }
+
+        const html = await response.text();
+        return html;
+    } catch (error) {
+        console.error("Error:", error);
+        return null;
     }
-
-    const html = await response.text();
-    return html;
-  } catch (error) {
-    console.error("Error:", error);
-    return null;
-  }
 }
 
 async function getTags(html: string): Promise<MetaTags[]> {
-  try {
-    // Create a DOM from the HTML string using jsdom
-    const dom = new JSDOM(html);
-    const doc = dom.window.document;
+    try {
+        // Create a DOM from the HTML string using jsdom
+        const dom = new JSDOM(html);
+        const doc = dom.window.document;
 
-    // Query the document for all meta tags
-    const metaTags = doc.querySelectorAll("meta");
+        // Query the document for all meta tags
+        const metaTags = doc.querySelectorAll("meta");
 
-    // Create an array to store the meta tag attributes
-    const metaTagAttributes: Array<{ [key: string]: string }> = [];
+        // Create an array to store the meta tag attributes
+        const metaTagAttributes: Array<{ [key: string]: string }> = [];
 
-    // Loop through each meta tag and extract attributes
-    metaTags.forEach((tag: Element) => {
-      const attributes: { [key: string]: string } = {};
-      tag.getAttributeNames().forEach((attributeName: string) => {
-        attributes[attributeName] = tag.getAttribute(attributeName) || "";
-      });
-      metaTagAttributes.push(attributes);
-    });
+        // Loop through each meta tag and extract attributes
+        metaTags.forEach((tag: Element) => {
+            const attributes: { [key: string]: string } = {};
+            tag.getAttributeNames().forEach((attributeName: string) => {
+                attributes[attributeName] = tag.getAttribute(attributeName) || "";
+            });
+            metaTagAttributes.push(attributes);
+        });
 
-    return metaTagAttributes;
-  } catch (error) {
-    console.error("Error parsing HTML:", error);
-    return [];
-  }
+        return metaTagAttributes;
+    } catch (error) {
+        console.error("Error parsing HTML:", error);
+        return [];
+    }
 }
+
 
 export { getTags, getHtml };
