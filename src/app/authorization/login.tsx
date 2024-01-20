d922777653947e3e192a7d894747ca5acb080334
Sanchithasharma <sanchitha.s.r@gmail.com>
2024-01-20 14:28:50 +1000

    added login and signup UI

---
+++ src/app/authorization/login.tsx
@@ -0,0 +1,71 @@
+"use client";
+import { useState } from "react";
+
+export default function Login() {
+    const [email, setEmail] = useState("jane.doe@gmail.com");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(event)
+        event.preventDefault();
+      }
+    
+
+  return (
+    <main className="flex flex-col items-center justify-between p-24">
+      <div className="w-full">
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 bg-brown-300 w-[80%]" onSubmit={handleSubmit}>
+          <h1>Log in</h1>
+          <br/>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              placeholder="Please enter your email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value)
+              }}
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="******************"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value)
+              }}
+            />
+            <p className="text-red-500 text-xs italic">
+              Please choose a password.
+            </p>
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-brown-200 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Log In
+            </button>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
+// import Login from '../app/authorization/login';
+import SignUp from '../app/authorization/signup';
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-      <h1 className="text-brown-400">Agency X</h1>
+      {/* <h1 className="text-brown-400">Agency X</h1> */}
+      {/* <Login/> */}
+      <SignUp/>
     </main>
   );
 }
