7e6dde38515d771a0825e5271e05ece02652c945
Mrkiller2001 <57162352+Mrkiller2001@users.noreply.github.com>
2024-01-21 14:53:06 +1000

    sign in stylised

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -46,66 +46,68 @@ export default function SignUp() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <form className="w-full max-w-sm" onSubmit={handleSubmit}>
-        <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3">
-            <label
-              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-              htmlFor="nline-email"
-            >
-              Email
-            </label>
-          </div>
-          <div className="md:w-2/3">
-            <input
-              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-              id="inline-email"
-              name="inline-email"
-              type="email"
-              placeholder="Please enter your email"
-              value={email}
+      <div className="w-full">
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="nline-email"
+              >
+                Email
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="inline-email"
+                name="inline-email"
+                type="email"
+                placeholder="Please enter your email"
+                value={email}
                 onChange={(event) => {
-                    setEmail(event.target.value)
+                  setEmail(event.target.value)
                 }}
-            />
+              />
+            </div>
           </div>
-        </div>
 
-        <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3">
-            <label
-              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-              htmlFor="inline-password"
-            >
-              Password
-            </label>
-          </div>
-          <div className="md:w-2/3">
-            <input
-              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-              id="inline-password"
-              type="password"
-              placeholder="******************"
-              value={password}
-              onChange={(event) => {
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="inline-password"
+              >
+                Password
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="inline-password"
+                type="password"
+                placeholder="******************"
+                value={password}
+                onChange={(event) => {
                   setPassword(event.target.value)
-              }}
-            />
+                }}
+              />
+            </div>
           </div>
-        </div>
 
-        <div className="md:flex md:items-center">
-          <div className="md:w-1/3"></div>
-          <div className="md:w-2/3">
-            <button
-              className="shadow bg-brown-200 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-              type="submit"
-            >
-              Sign Up
-            </button>
+          <div className="md:flex md:items-center">
+            <div className="md:w-1/3"></div>
+            <div className="md:w-2/3">
+              <button
+                className="shadow bg-brown-200 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                type="submit"
+              >
+                Sign Up
+              </button>
+            </div>
           </div>
-        </div>
-      </form>{" "}
+        </form>{" "}
+      </div>
     </main>
   );
 }
