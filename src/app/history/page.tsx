0fd80fc30bd32abc895a2d5de5fbcaf0a55605dc
Mrkiller2001 <57162352+Mrkiller2001@users.noreply.github.com>
2024-01-21 13:49:33 +1000

    can now loggout and state persists

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -3,11 +3,11 @@ import login from "../../firebase/login";
 import { useRouter } from "next/navigation";
 
 import { FormEvent, useEffect, useState } from "react";
-
+import { signOut } from "../../firebase/signout";
 import { DialogBox } from "../../components/dialog";
 import { db } from "../../firebase/firestore";
 import { collection, getDocs, setDoc } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import firebase_app from "../../../config";
 
 const TABLE_HEADER = ["URL", "View"];
@@ -61,7 +61,39 @@ export default function History() {
   // refer to the implementation on the main app page
   const [documents, setDocuments] = useState<any[]>([]);
   const [open, setOpen] = useState(false);
+  const [user, setUser] = useState(getAuth(firebase_app).currentUser);
+  const auth = getAuth(firebase_app);
 
+  const router = useRouter();
+  console.log({ loggedInUser: user?.email });
+
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        localStorage.setItem("loggedInUser", JSON.stringify(user));
+      } else {
+        localStorage.removeItem("loggedInUser");
+      }
+      setUser(user);
+      if (!user) {
+        router.push("/login");
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [auth, router]);
+  const handleLogout = async () => {
+    try {
+      await signOut();
+      // The onAuthStateChanged will handle updating the user state
+      // and redirecting if necessary.
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
+  };
   useEffect(() => {
     const fetchDataAndNames = async () => {
       const loggedInUser = await getAuth(firebase_app);
@@ -78,7 +110,15 @@ export default function History() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <h1>Past Reports</h1>
-
+      {user && (
+        <button
+          type="button"
+          onClick={handleLogout}
+          className="bg-brown-200 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Logout
+        </button>
+      )}
       <div className="p-8">
         <table className="table-auto border p-2">
           <thead>
---
+++ src/firebase/signout.ts
@@ -0,0 +1,9 @@
+import firebase_app from "../../config";
+import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+
+const auth = getAuth(firebase_app);
+
+
+export const signOut = async () => {
+    await auth.signOut();
+};
