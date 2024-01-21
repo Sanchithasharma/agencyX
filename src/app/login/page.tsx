2965ad2ef102ba92ecf107ee6ecae9c2a25b004f
Sanchithasharma <sanchitha.s.r@gmail.com>
2024-01-21 14:40:47 +1000

    removed border for password

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -85,7 +85,7 @@ export default function Login() {
               Password
             </label>
             <input
-              className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
               id="password"
               type="password"
               placeholder="******************"
