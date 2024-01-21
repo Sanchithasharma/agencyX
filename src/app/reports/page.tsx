fe1c2b5541f5a54fa01e66d948db2e9cf52e2e7f
Brian Riwu Kaho <b.riwukaho@gmail.com>
2024-01-21 11:29:25 +1000

    complete all document fetch

--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -1,30 +1,38 @@
 "use client";
 import signUp from "../../firebase/signup";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 import { FormEvent, useEffect, useState } from "react";
-import { db }  from "../../firebase/firestore";
-import { collection, doc, getDoc } from "firebase/firestore"; 
+import { db } from "../../firebase/firestore";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 
-
-export default function Report() {
-    useEffect(() => {
-        const fetchData = async () => {
-            const getRef = doc(db, "evgenia@mail.com", "heroicons.com%252F");
-            const docSnap = await getDoc(getRef);
-            if (docSnap.exists()) {
-                console.log("Document data:", docSnap.data());
-            } else {
-                // doc.data() will be undefined in this case
-                console.log("No such document!");
-            }
-        };
-        fetchData();
-    })
-
-    return(
-        <div>
-            <h1>Report</h1>
-        </div>
-    )
+async function getAllDocumentNames(collectionName: string) {
+  const collectionRef = collection(db, collectionName);
+  const snapshot = await getDocs(collectionRef);
+  const documentNames = snapshot.docs.map((doc) => doc.id);
+  return documentNames;
 }
 
+export default function Report() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const getRef = doc(db, "evgenia@mail.com", "heroicons.com%2F");
+      const docSnap = await getDoc(getRef);
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+      const docNames = await getAllDocumentNames("evgenia@mail.com");
+      console.log("Document names", docNames);
+    };
+
+    fetchData();
+  });
+
+  return (
+    <div>
+      <h1>Report</h1>
+    </div>
+  );
+}
